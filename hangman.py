9c1a69d1d901918de2588d5a49abfee6ff2ed5eb
YashSingh15 <83363915+YashSingh15@users.noreply.github.com>
2021-09-10 10:48:52 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,15 +3,15 @@ import random
 words = ['Sun', 'Moon', 'Earth', 'Planet', 'Galaxy', 'Star', 'Cosmos', 'Stellar', 'Astro']  #Astronomy themed words
 word = random.choice(words)
 
-lowercase_word = word.lower()
+lowercase_word = word.lower()   #converts all letters to lowercase for easier comparison with user input
 
 list_word = list(word)
 
 for letter_count in range(len(list_word)):
-    list_word[letter_count] = '_'
+    list_word[letter_count] = '_'   #converts all letters in the chosen word to underscores equal to the number of letters in the word
 
 
-def list_to_str(my_list):
+def list_to_str(my_list):   #converts strings to lists, I did this because strings are immmutable in Python, but I'm sure there's a better way to do this
     string = ""
     for letter in my_list:
         string += letter
@@ -30,7 +30,7 @@ life = starting_life
 guessed_letters = []
 
 while life > 0:
-    check_letter = input("\nGuess a letter: ").lower()
+    check_letter = input("\nGuess a letter: ").lower()  #Again, converting everything to lowercase for easier comparison
     if check_letter == '':
         print("Character not entered. Try again.")
     elif check_letter in lowercase_word:
@@ -39,17 +39,17 @@ while life > 0:
         for letter_count in range(len(list_word)):
             if list_word[letter_count].lower() == check_letter or list_word[letter_count] in guessed_letters:
                 continue
-            list_word[letter_count] = '_'
+            list_word[letter_count] = '_'   #This loop replaces every letter other than what was just guessed or has been already guessed in previous iterations by an underscore
 
-        updated_unknown_word = list_to_str(list_word)
-        print(updated_unknown_word)
+        updated_unknown_word = list_to_str(list_word)   #this variable represents the updated list, with some underscores and some guessed letters to show progress
+        print(updated_unknown_word
 
         for i in range(len(updated_unknown_word)):
             if updated_unknown_word[i] != '_':
-                guessed_letters += updated_unknown_word[i]
+                guessed_letters += updated_unknown_word[i]  #adds the letter which was just guessed to the list of already guessed letters
 
         if '_' not in updated_unknown_word:
-            break
+            break   #exits the loop if no unknown letters remaning
 
     else:
         print("Oops! Wrong guess! You lose one life.")
